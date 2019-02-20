b8213627afb7753d8e40e1c0f3485485dc7cf964
TyDSmith <36959531+TyDSmith@users.noreply.github.com>
2019-02-19 20:30:07 -0800

    gem score increase functionality added

---
+++ assets/javascript/game.js
@@ -0,0 +1,63 @@
+let wins = 0;
+let loses = 0;
+let score = 0;
+let valueGoal;
+
+let blueGem = 0;
+let redGem = 0;
+let orangeGem = 0;
+let greenGem = 0;
+
+
+function setup(){
+    var x = $(".target-num");
+    var valueGoal = Math.floor(Math.random() * (120 - 19) + 19);
+    x.html(valueGoal)
+    var blueGem = Math.floor(Math.random() * (12 - 1) + 1);
+    var redGem = Math.floor(Math.random() * (12 - 1) + 1);
+    var orangeGem = Math.floor(Math.random() * (12 - 1) + 1);
+    var greenGem = Math.floor(Math.random() * (12 - 1) + 1);
+    while(blueGem === greenGem) {
+      var blueGem = Math.floor(Math.random() * (12 - 1) + 1);
+    }
+    while(redGem === greenGem || redGem === blueGem) {
+      var redGem = Math.floor(Math.random() * (12 - 1) + 1);
+    }
+    while(orangeGem === greenGem || orangeGem === redGem || orangeGem === blueGem) {
+      var orangeGem = Math.floor(Math.random() * (12 - 1) + 1);
+    }
+    console.log(blueGem);
+    console.log(redGem);
+    console.log(orangeGem);
+    console.log(greenGem);
+
+
+
+$("#bluecrystal").click(addBlue);
+function addBlue(){
+    var x = $("#score-num");
+    score = score + blueGem;
+    x.html(score);
+};
+
+$("#redcrystal").click(addRed);
+function addRed(){
+    var x = $("#score-num");
+    score = score + redGem;
+    x.html(score);
+};
+
+$("#orangecrystal").click(addOrange);
+function addOrange(){
+    var x = $("#score-num");
+    score = score + orangeGem;
+    x.html(score);
+};
+
+$("#greencrystal").click(addGreen);
+function addGreen(){
+    var x = $("#score-num");
+    score = score + greenGem;
+    x.html(score);
+};
+};
